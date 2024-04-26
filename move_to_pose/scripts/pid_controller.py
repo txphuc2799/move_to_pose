7273b672f10d83fb80b0bd6901ffb941b7483a5a
tannhat22 <nguyentannhat2298@gmail.com>
2024-04-26 18:13:40 +0700

    modify

--- move_to_pose/scripts/pid_controller.py
+++ move_to_pose/scripts/pid_controller.py
@@ -1,7 +1,7 @@
 class PIDController(object):
     
-    def __init__(self, T, kP=0.0, kI=0.0, kD=0.0):
-        self.T = T
+    def __init__(self, dt=0.1, kP=0.0, kI=0.0, kD=0.0):
+        self.dt = dt
         self.kP = kP
         self.kI = kI
         self.kD = kD
@@ -9,8 +9,8 @@ class PIDController(object):
         self.integral = 0.0
 
     def compute(self, error):
-        self.integral += error * self.T
-        derivative = (error - self.prev_error) / self.T
+        self.integral += error * self.dt
+        derivative = (error - self.prev_error) / self.dt
         output = self.kP * error + self.kI * self.integral + self.kD * derivative
         self.prev_error = error
 
